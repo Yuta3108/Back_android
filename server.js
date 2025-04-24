6e5bfffa735e348f807e2317ef31b73afaee8d27
Yuta3108 <toyotran0@gmail.com>
2025-04-24 22:10:32 +0700

    check

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./dtb.env" });
+require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const mysql = require("mysql2");
 const jwt = require("jsonwebtoken");
