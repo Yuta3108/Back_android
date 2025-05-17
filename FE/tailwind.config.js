50dd959928f4ebeccad4c67ed90ece101ba1c9b5
san-git-sudo <st857146@gmail.com>
2025-05-17 11:30:59 +0700

    cau hinh FE

---
+++ FE/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",  // chỉnh theo thư mục source của anh
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
