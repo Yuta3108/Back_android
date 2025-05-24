c4e7882e717f137e0975465b205e0626b36b4f97
Yuta3108 <toyotran0@gmail.com>
2025-05-24 15:04:25 +0700

    sua ty

--- server.js
+++ server.js
@@ -92,9 +92,10 @@ app.post("/api/login", (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: "1h" }
         );
-
-        res.json({ message: "Đăng nhập thành công!", token });
+       
+        res.json({ message: "Đăng nhập thành công!", token ,userId: user.id});
     });
+    
 });
 //api nhân viên
 app.use('/api/employees', employeeRoutes);
