b598646813a2f911064f52bd3819f59867b8cb9d
san-git-sudo <st857146@gmail.com>
2025-05-27 16:31:20 +0700

    sua xong loi hien thi khach hang ben dashboard

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+// Thống kê khách hàng
+router.get('/users/stats', usersController.getCustomerStats);
+
+// Route lấy thông tin chi tiết user theo id
+router.get('/users/:id', usersController.getUserById);
+
+// Route lấy danh sách khách hàng
+router.get('/users', usersController.getAllUsers);
 
 // Route cập nhật user
 router.put('/users/:id', usersController.updateUser);
 
-// Route lấy danh sách khách hàng
-router.get('/users', usersController.getAllUsers);
-// Route lấy thông tin chi tiết user theo id
-router.get('/users/:id', usersController.getUserById);
-
-
 // Route xoá khách hàng
 router.delete('/users/:id', usersController.deleteUser);
 
-// Thống kê khách hàng
-router.get('/users/stats', usersController.getCustomerStats);
 
 module.exports = router;
